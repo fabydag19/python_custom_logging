09faf9a42c84c49d9d9b0a8418b2b695e86cc1a5
fabydag19 <62938446+fabydag19@users.noreply.github.com>
2023-11-21 10:07:52 +0100

    Update example_same_directory.py

--- Custom Log/example_same_directory.py
+++ Custom Log/example_same_directory.py
@@ -1,6 +1,6 @@
 #If the log's file is in the same directory of custom_log (configuration), you do this
 import logging
-from test import CustomLog
+from custom_log import CustomLog
 
 #Set file, level, text
 logpy('example.log', 'info', 'This is an example log')
