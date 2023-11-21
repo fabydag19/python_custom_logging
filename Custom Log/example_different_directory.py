b286b782d159e74d08b50c430a234be9ba94bf10
fabydag19 <62938446+fabydag19@users.noreply.github.com>
2023-11-21 10:40:45 +0100

    Update example_different_directory.py

--- Custom Log/example_different_directory.py
+++ Custom Log/example_different_directory.py
@@ -1,8 +1,8 @@
-#If the script is in a the different directory than custom_log.py (configuration), you do this
+# If the script is in a the different directory than custom_log.py (configuration), you do this
 import logging
 import sys
 sys.path.append('personal/directory/log/config/') #add position of log configuration
 from custom_log import CustomLog
 
-#Set file, level, text
+# Set file, level, text
 CustomLog('example.log', 'info', 'This is an example log')
