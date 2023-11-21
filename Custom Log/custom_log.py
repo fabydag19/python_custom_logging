72b872770e43824bcc72b475bff01e7513075103
fabydag19 <62938446+fabydag19@users.noreply.github.com>
2023-11-21 10:27:27 +0100

    Update custom_log.py

--- Custom Log/custom_log.py
+++ Custom Log/custom_log.py
@@ -7,15 +7,15 @@ def CustomLog(log_file, log_level, log_text):
     # Check if handler already exist
     if not logger.handlers:
         logger.setLevel(logging.DEBUG) # Set min threshold log level
-        handler = logging.FileHandler(f'{log_file}_.log') # Generic log file
-        #handler = logging.FileHandler(datetime.now().strftime(f'{log_file}_%d_%m_%Y.log')) # Single day log file
+        handler = logging.FileHandler(f'{log_file}_.log') # Common log file
+        handler = logging.FileHandler(datetime.now().strftime(f'{log_file}_%d_%m_%Y.log')) # Single day log file
         formatter = logging.Formatter(fmt='%(asctime)s.%(msecs)03d - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
         handler.setFormatter(formatter)
         logger.addHandler(handler)
     else:
         pass
     
-    # Check log's level
+    # Check log level
     if log_level in ['debug', 'DEBUG']:
         logger.debug(log_text)
     elif log_level in ['info', 'INFO']:
